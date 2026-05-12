9bcf8d3c62ccf2d528a8509c04ef61c5a5c19168
Quin Gillespie <trypsynth@gmail.com>
2026-05-12 07:02:20 -0600

    Vendor the chmlib c sources to avoid 503s.

--- build.rs
+++ build.rs
@@ -1,20 +1,9 @@
-use std::{
-	env, fs,
-	io::Cursor,
-	path::{Path, PathBuf},
-};
+use std::{fs, path::PathBuf};
 
-use bzip2::read::BzDecoder;
 use cc::Build;
-use tar::Archive;
 
 fn main() {
-	let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-	let chmlib_dir = out_dir.join("chmlib-0.40");
-	let src_dir = chmlib_dir.join("src");
-	if !chmlib_dir.exists() {
-		download_and_extract(&out_dir);
-	}
+	let src_dir = PathBuf::from("vendor/chmlib/src");
 	apply_patches(&src_dir);
 	let mut build = Build::new();
 	build.file(src_dir.join("chm_lib.c")).file(src_dir.join("lzx.c")).include(&src_dir).warnings(false);
@@ -28,16 +17,7 @@ fn main() {
 	println!("cargo:rustc-link-lib=static=chm");
 }
 
-fn download_and_extract(out_dir: &Path) {
-	let url = "http://www.jedrea.com/chmlib/chmlib-0.40.tar.bz2";
-	let response = ureq::get(url).call().expect("Failed to download chmlib");
-	let buf = response.into_body().read_to_vec().expect("Failed to read chmlib tarball");
-	let decompressor = BzDecoder::new(Cursor::new(buf));
-	let mut archive = Archive::new(decompressor);
-	archive.unpack(out_dir).expect("Failed to extract chmlib");
-}
-
-fn apply_patches(src_dir: &Path) {
+fn apply_patches(src_dir: &std::path::Path) {
 	let chm_lib_path = src_dir.join("chm_lib.c");
 	let mut contents = fs::read_to_string(&chm_lib_path).expect("Failed to read chm_lib.c");
 	contents = contents.replace("/* yielding an error is preferable to yielding incorrect behavior */\n#error \"Please define the sized types for your platform in chm_lib.c\"", "typedef unsigned char           UChar;\ntypedef int16_t                 Int16;\ntypedef uint16_t                UInt16;\ntypedef int32_t                 Int32;\ntypedef uint32_t                UInt32;\ntypedef int64_t                 Int64;\ntypedef uint64_t                UInt64;");
